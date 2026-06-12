65bd196353cf0f7b7072931cceb8a2ed40c4a70e
Anika <anika.seier@web.de>
2026-06-12 15:16:24 +0200

    Navigation: Besonderheit Drop-Down-Menü Schrift

--- script.js
+++ script.js
@@ -22,12 +22,24 @@ function setActiveNav() {
     const page = document.body.dataset.page;
     if (!page) return;
 
+    // 1. direkte Links aktivieren
     const link = document.querySelector(
         `.nav-button[href="${page}.html"]`
     );
 
     if (link) {
         link.classList.add("active");
+    }
+
+    // 2. Dropdown-Gruppe "Schrift" aktivieren
+    const schriftPages = ["schrift", "hiragana", "katakana", "kanji"];
+
+    if (schriftPages.includes(page)) {
+        const schriftButton = document.getElementById("schriftButton");
+
+        if (schriftButton) {
+            schriftButton.classList.add("active");
+        }
     }
 }
 
